38f8142ebda9385cf8138067658531337c438434
Rajesh <tipstricks118@gmail.com>
2024-01-02 22:00:25 +0545

    fixed the bug related to the product entity

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
 import { InventoryEntity } from './entities/inventory.entity';
 import { ChannelEntity } from 'src/channels/entities/channel.entity';
+import { OrderEntity } from 'src/orders/entities/order.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProductEntity]),
     TypeOrmModule.forFeature([InventoryEntity]),
     TypeOrmModule.forFeature([ChannelEntity]),
+    TypeOrmModule.forFeature([OrderEntity]),
   ],
   controllers: [ProductController],
   providers: [ProductsService],
