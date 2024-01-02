bd1528b4da6714599346d1e8079ebc9b50308a41
Bikash <thebikashpokhrel@gmail.com>
2024-01-02 10:52:31 +0545

    Initial repo

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import sidebar from "./Sidebar.module.css"
+
+const Sidebar = function(){
+    return (
+        <div>
+            <ul>
+                <li>Dashboard</li>
+                <li>Products</li>
+                <li>Orders</li>
+                <li>Channels</li>
+                <li>Ask AI</li>
+            </ul>
+        </div>
+    )
+}
