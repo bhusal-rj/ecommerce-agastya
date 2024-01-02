c0b220b71cc05bb86cdf75e83f90eacd03d7f3f2
Rajesh bhusal <33444884+bhusal-rj@users.noreply.github.com>
2024-01-02 09:38:02 +0545

    Delete dashboard/frontend directory

--- dashboard/frontend/src/apiservices/index.js
+++
@@ -1,6 +0,0 @@
-import { deleteRequest, getRequest, postRequest, putRequest } from "./fetch";
-
-export const getOrders = async () => {
-    const res = await getRequest("/orders/all");
-    return res.data;
-  };
