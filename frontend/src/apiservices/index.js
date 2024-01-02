c40d88f9d5f1b2888b4a57c2f7d857d0c451c562
Bikash <thebikashpokhrel@gmail.com>
2024-01-02 20:09:11 +0545

    Products View added

--- frontend/src/apiservices/index.js
+++ frontend/src/apiservices/index.js
@@ -1,13 +1,24 @@
 import { deleteRequest, getRequest, postRequest, putRequest } from "./fetch";
 
+export const getProducts = async() =>{
+  const res = await getRequest("/products");
+  return res.data;
+}
+
 export const getOrders = async () => {
     const res = await getRequest("/orders/all");
     return res.data;
   };
   export const getChannels = async () => {
-    const res = await getRequest("/channels/all");
+    const res = await getRequest(`/channels/`);
     return res.data;
   };
+
+  export const getChannel = async (id) => {
+    const res = await getRequest(`/channel/${id}`);
+    return res.data;
+  };
+
   export const AddProduct=async()=>{
     console.log("added product");
   }
